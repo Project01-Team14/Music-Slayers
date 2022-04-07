b22984098dcb0ec2cfe6a3a24f0e8810aaa0dbe1
Yurippe <yurichikawa1992@gmail.com>
2022-04-07 13:34:20 -0400

    add ability to create necessary elements to show graphs and get/remove localStorage data

--- assets/js/listeners-graph.js
+++ assets/js/listeners-graph.js
@@ -1,47 +1,13 @@
-var artistId = $(this).attr("artistId").substr(15);
-const options = {
-method: "GET",
-headers: {
-    "X-RapidAPI-Host": "spotify23.p.rapidapi.com",
-    "X-RapidAPI-Key": "8953cc16a8msh362e3da83f41059p119f26jsn264a41186942",
-},
-};
+var graphData = JSON.parse(localStorage.getItem("graph-data"));
 
-fetch(
-"https://spotify23.p.rapidapi.com/artist_overview/?id=" + artistId,
-options
-)
-.then((response) => response.json())
-.then(function (data) {
-    console.log(data);
-    var topCities = data.data.artist.stats.topCities;
+var chartEl = $("<div>")
+    .attr("id", "chart-div")
+var spaceEl = $("<br/>")
+    .attr("id", "space")  
+var btnEl = $("<div>")
+    .attr("id", "btn-group")
+$("#graph-container").append(chartEl, spaceEl, btnEl);
 
-    var citiesUsers = [];
+console.log(graphData);
 
-    for (var i = 0; i < 3; i++) {
-    var numberOfListeners = topCities.items[i].numberOfListeners;
-    var listenersByCity = topCities.items[i].city;
-    var tempArr = {
-        numbers: numberOfListeners,
-        city: listenersByCity,
-    };
-    citiesUsers.push(tempArr);
-    }
-
-
-
-//   var graphContEl = $("<div>")
-//         .addClass("graph-container")
-//     var chartEl = $("<div>")
-//         .attr("id", "chart-div")
-//     var spaceEl = $("<br/>")
-//         .attr("id", "space")  
-//     var btnEl = $("<div>")
-//         .attr("id", "btn-group")
-//     graphContEl.append(chartEl, spaceEl, btnEl);
-
-
-    console.log($(this));
-    console.log(citiesUsers);
-})
-.catch((err) => console.error(err));
+localStorage.removeItem("graph-data");
