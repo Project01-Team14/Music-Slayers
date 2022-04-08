bcb265e41a8e3e77ac2a3f04dcdd7071fc78def2
Yurippe <yurichikawa1992@gmail.com>
2022-04-08 17:26:41 -0400

    removed radio buttons and refactored codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,65 +29,14 @@ var getSearch = function (searchCriteria) {
   )
     .then((response) => response.json())
     .then(function (response) {
-      console.log(response);
-      if (selectedBtn === "Songs") {
-        searchBySong(response);
-      }
+        createSongList(response);
     })
     // if error show the error
     .catch((err) => console.error(err));
-};
-
-// fetch data from deezer
-var getMusicData = function (search, artist) {
-  // set options to fetch url
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-      // YURI'S API KEY
-      "X-RapidAPI-Key": "6c661726cemsh8e0e6330646001dp18ca72jsndcad811f5f1d",
-    },
-  };
-
-  // get data by fetching deezer url using user's criteria
-  fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + search, options)
-    .then((response) => response.json())
-    // get data
-    .then(function (response) {
-      // GET PREVIEW DATA (NOT COMPLETED)
-      for (var i = 0; i < response.data.length; i++) {
-        var deezerData = response.data[i];
-        if (deezerData.title === search) {
-          if (deezerData.artist.name === artist) {
-            songPreview = deezerData.preview;
-            break;
-          }
-        }
-      }
-    })
-    // if error show the error
-    .catch((err) => console.error(err));
-};
-
-var searchEventHandler = function (searchCriteria) {
-  // if "Songs" radio button is selected, get tracks data
-  if (selectedBtn === "Songs") {
-    getSearch(searchCriteria);
-    // if "Artists" selected, get artists data
-  } else if (selectedBtn === "Artists") {
-    searchByArtists(response);
-    // if "Albums" selected, get artists data
-  } else if (selectedBtn === "Albums") {
-    searchByAlbums(response);
-    // if nothing selected, show error
-  } else {
-    console.log("error");
-  }
 };
 
 // search by "Songs"
-var searchBySong = function (response) {
+var createSongList = function (response) {
   if ($(".song-list") !== null) {
     $(".song-list").detach();
   }
@@ -110,7 +59,7 @@ var searchBySong = function (response) {
     };
     console.log(resultData);
 
-    createSpotSongList(resultData, listEl);
+    createSongListElements(resultData, listEl);
   }
 
   $("#display-container").append(listEl);
@@ -129,73 +78,7 @@ var searchBySong = function (response) {
   }
 };
 
-// search by "Artists"
-var searchByArtists = function (response) {
-  if ($(".results-container") !== null) {
-    $(".results-container").detach();
-  }
-
-  resultsEl = $("<div>").addClass("results-container");
-
-  for (var i = 0; i < 10; i++) {
-    data = response.data[i];
-
-    if (data.artist.name.match(searchCriteria)) {
-      createSongList(data, resultsEl);
-    } else {
-      console.log("not the one");
-    }
-  }
-
-  $("#display-container").append(resultsEl);
-
-  var checkElement = document.querySelector(".song-list");
-
-  if (checkElement) {
-    $("#result-subtitle").html(
-      "Showing results for: " + searchCriteria + " (" + selectedBtn + ")"
-    );
-  } else {
-    $("#result-subtitle").html(
-      "No results found for: " + searchCriteria + " (" + selectedBtn + ")"
-    );
-  }
-};
-
-// search by "Albums"
-var searchByAlbums = function (response) {
-  if ($(".results-container") !== null) {
-    $(".results-container").detach();
-  }
-
-  resultsEl = $("<div>").addClass("results-container");
-
-  for (var i = 0; i < 10; i++) {
-    data = response.data[i];
-
-    if (data.album.title.match(searchCriteria)) {
-      createSongList(data, resultsEl);
-    } else {
-      console.log("not the one");
-    }
-  }
-
-  $("#display-container").append(resultsEl);
-
-  var checkElement = document.querySelector(".song-list");
-
-  if (checkElement) {
-    $("#result-subtitle").html(
-      "Showing results for: " + searchCriteria + " (" + selectedBtn + ")"
-    );
-  } else {
-    $("#result-subtitle").html(
-      "No results found for: " + searchCriteria + " (" + selectedBtn + ")"
-    );
-  }
-};
-
-var createSpotSongList = function (data, listEl) {
+var createSongListElements = function (data, listEl) {
   var listItemEl = $("<li>").addClass("song-item");
 
   var albumImageEl = $("<img>")
@@ -242,68 +125,17 @@ var createSpotSongList = function (data, listEl) {
     listenersByCity
   );
   listEl.append(listItemEl);
-
-  // console.log(data);
 };
-
-// var createSongList = function (data, resultsEl) {
-//   var listEl = $("<ul>").addClass("song-list");
-//   var listItemEl = $("<li>").addClass("song-item");
-
-//   var albumImageEl = $("<img>")
-//     .attr("src", data.album.cover_small)
-//     .attr("alt", data.artist.name + "'s album " + data.album.title);
-//   var songTitleEl = $("<h3>").addClass("song-title").html(data.title);
-//   var otherInfoEl = $("<p>")
-//     .addClass("song-info")
-//     .html(data.artist.name + " - " + data.album.title);
-//   var lyricsBtnEl = $("<button>")
-//     .attr("type", "button")
-//     .addClass("lyrics-btn")
-//     .html("Lyrics");
-//   var playBtnEl = $("<button>")
-//     .attr("type", "button")
-//     .attr("data-play", data.preview)
-//     .addClass("play-btn")
-//     .html("Play");
-
-//   listItemEl.append(
-//     albumImageEl,
-//     songTitleEl,
-//     otherInfoEl,
-//     lyricsBtnEl,
-//     playBtnEl
-//   );
-//   listEl.append(listItemEl);
-//   resultsEl.append(listEl);
-
-//   console.log(data);
-//   console.log(data.artist.picture_small);
-// };
 
 //Add event listener button
 buttonEl.addEventListener("click", function (event) {
   event.preventDefault();
   // get user's search criteria
   searchCriteria = searchEl.value;
-  searchCriteria =
-    searchCriteria.charAt(0).toUpperCase() + searchCriteria.slice(1);
+  searchCriteria = searchCriteria.charAt(0).toUpperCase() + searchCriteria.slice(1);
   console.log(searchCriteria);
-
-  // select all radio buttons
-  var radioButtons = document.querySelectorAll("input[name='lyrics']");
-
-  // search for radio button user selected
-  for (var i of radioButtons) {
-    // if button is checked, get its value
-    if (i.checked) {
-      selectedBtn = i.value;
-      //   getMusicData(searchCriteria, selectedBtn);
-      searchEventHandler(searchCriteria);
-      break;
-    }
-  }
-  console.log(selectedBtn);
+  
+  getSearch(searchCriteria);
 });
 
 $("#display-container").on("click", ".play-btn", function () {
@@ -349,14 +181,7 @@ $("#display-container").on("click", ".play-btn", function () {
   }
 });
 
-//set options to fetch lyrics url
-
-// var getLyrics = function(trackId) {
-
-//   }
-// console.log(lyricsLinkEl);
-// var checkElement = document.querySelector(".show-lyrics");
-// console.log(checkElement);
+//fetch lyrics
 $("#display-container").on("click", ".lyrics-btn", function () {
   // var showLyrics = $(this).attr.text(displayLyrics);
   var trackId = $(this).attr("trackId").substr(14);
@@ -373,32 +198,24 @@ $("#display-container").on("click", ".lyrics-btn", function () {
 
   fetch("https://spotify23.p.rapidapi.com/track_lyrics/?id=" + trackId, options)
     .then((response) => response.json())
-    .then(function (response) {
-      console.log(response);
-      showLyrics(response);
+    .then(function (data) {
+      var lyricsData = [];
+
+      for (var i = 0; i < data.lyrics.lines.length; i++) {
+        var lyricsLine = data.lyrics.lines[i].words;
+        var tempArr = {
+          line: lyricsLine,
+        };
+        lyricsData.push(tempArr);
+      }
+      console.log(lyricsData);
+
+      //store the lyrics data into local storage
+      localStorage.setItem("Lyrics", JSON.stringify(lyricsData));
     })
     // if error show the error
     .catch((err) => console.error(err));
-
-  // create click event for lyricsBtnEl
-  // console.log(getLyrics);
-  // search by Lyrics
 });
-
-var showLyrics = function (data) {
-  var lyricsData = [];
-  //   var lyricsData = data.response.lyrics.lyrics[i].body.html;
-  for (var i = 0; i < data.lyrics.lines.length; i++) {
-    var lyricsLine = data.lyrics.lines[i].words;
-    var tempArr = {
-      line: lyricsLine,
-    };
-    lyricsData.push(tempArr);
-  }
-  console.log(lyricsData);
-  //store the lyrics data into local storage
-  localStorage.setItem("Lyrics", JSON.stringify(lyricsData));
-};
 
 // displaying cities by users
 $("#display-container").on("click", ".country-btn", function (event) {
@@ -493,4 +310,4 @@ var globalTop10 = function () {
     .catch((err) => console.error(err));
 };
 
-globalTop10();
+// globalTop10();
